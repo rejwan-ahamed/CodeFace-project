4abba9e0268dbb3e50c1b81bc15306971ce77ece
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-09-20 16:57:10 +0600

    tab and desktop responsiveness add

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("sep 30, 2022").getTime();
+var countDownDate = new Date("sep 21, 2022").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
